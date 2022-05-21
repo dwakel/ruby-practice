a033e7613ea54ae24cf18bead92bbeec3d770c70
Selorm Avoke <savoke@acsghana.com>
2022-05-21 20:11:21 +0000

    Solutions to Calendar

--- calendar.rb
+++ calendar.rb
@@ -1,54 +1,68 @@
 require 'date'
 
+ # It is dangeraous to initialize array elements with a proc or a lambda, 
+    # This is because they could act like a closure that share a base object
+    # causing it to overwrite itselt
+    # I need to do more reading into this
+    # Hence we initialize the map manually.
+    # todo: need to look for other prgramatic initialization methods
+    #  calendar = {
+    #     0 => [' ',' ',' ',' ',' ',' ',' '],
+    #     1 => [' ',' ',' ',' ',' ',' ',' '],
+    #     2 => [' ',' ',' ',' ',' ',' ',' '],
+    #     3 => [' ',' ',' ',' ',' ',' ',' '],
+    #     4 => [' ',' ',' ',' ',' ',' ',' '],
+    #     5 => [' ',' ',' ',' ',' ',' ',' ']
+    # }
+    # Manual initialization is also not effective
+    # Method to use:
+    # calendar = Hash.new {|cal, key| cal[key] = [' ',' ',' ',' ',' ',' ',' '] }
+
 def cal(month, year)
-    t = Time.new(year, month)
-    month = Date.new(year, t.month)
+    month = Date.new(year, month)
+    first_wday = month.wday
     month_end = month.next_month - 1
-    # puts month_end.day
-    calendar = Array.new(5, [' ',' ',' ',' ',' ',' ',' '])
     vertical_index = 0
-    print "Su\t", "Mo\t", "Tu\t", "We\t", "Th\t", "Fr\t", "Sa\t\n"
-    (month.day..month_end.day).each do |day|
-        case Date.new(year, t.month, day).wday
-            when 0
-                # print calendar[vertical_index]
-                calendar[vertical_index][0] = day
-            when 1
-                # print day
-                calendar[vertical_index][1] = day
-            when 2
-                # print day
-                calendar[vertical_index][2] = day
-            when 3
-                # print day
-                calendar[vertical_index][3] = day
-            when 4
-                # print day
-                calendar[vertical_index][4] = day
-            when 5
-                # print day
-                calendar[vertical_index][5] = day
-            when 6
-                calendar[vertical_index][6] = day
-                vertical_index = vertical_index + 1
-               
-            else
-
-            end
-            
+    calendar = Hash.new {|cal, key| cal[key] = [' ',' ',' ',' ',' ',' ',' '] }
+    (month..month_end).each do |day|
+        wday = day.wday
+        calendar[vertical_index][wday] = day.day
+        vertical_index += 1 if wday == 6;
     end
-
-    calendar.each do |item|
-        item.each do |i|
+    print "Su\t", "Mo\t", "Tu\t", "We\t", "Th\t", "Fr\t", "Sa\t\n"
+    calendar.keys.sort.each do |item|
+        calendar[item].each do |i|
             printf("%s\t", i)
         end
         puts
     end
-    
 end
 
 cal(4, 2022)
 
 
-# calendar = Array.new(5, [' ',' ',' ',' ',' ',' ',' '])
+
+def cal_solution_2(month, year)
+    t = Time.new(year, month)
+    month = Date.new(year, t.month)
+    first_wday = month.wday
+    month_end = month.next_month - 1
+    calendar = {}
+    (month..month_end).each do |day|
+        wday = day.wday
+        calendar[wday] = [] if calendar[wday] == nil
+        calendar[wday].push(day.day)
+       
+    end
+    calendar.each{|k,v| v.unshift " " if first_wday != 0 && k < first_wday}
+    print "Su\t", "Mo\t", "Tu\t", "We\t", "Th\t", "Fr\t", "Sa\t\n"
+    (0..5).each do |w|
+        calendar.keys.sort.each do |item|
+            printf("%s\t", calendar[item][w])
+        end
+        puts
+    end
+end
+
+#  cal_solution_2(1, 2022)
 
